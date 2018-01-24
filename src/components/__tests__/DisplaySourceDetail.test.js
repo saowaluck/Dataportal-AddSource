bec9ec6f7535b0cef4597d971d2c0ec01f4b0c17
Thanisorn <thanisorn0124@gmail.com>
2018-01-25 02:10:39 +0700

    Revise Test  Please enter the commit message for your changes. Lines starting

--- src/components/__tests__/DisplaySourceDetail.test.js
+++ src/components/__tests__/DisplaySourceDetail.test.js
@@ -16,21 +16,38 @@ describe('< DisplaySourceDetail />', () => {
     const name = 'Track Reseller'
     const type = 'Superset Dashboard'
     const url = 'https://www.prontotools.io/'
+    const tag = 'Reseller'
+    const dateofCreate = '24, 01, 2018'
+    const creator = 'kan ouivirus'
 
-    const wrapper = shallow(<DisplaySourceDetail name={name} type={type} url={url} />)
-    expect(wrapper.html()).toEqual('<div class="ui container">' +
-      '<div class="ui stackable grid">' +
-      '<div class="eleven wide column">' +
-      '<div class="ui segment">' +
-      '<iframe src="https://www.prontotools.io/" title="Track Reseller" frame-ancestors="none" width="100%" height="580px"></iframe>' +
-      '</div>' +
-      '</div>' +
-      '<div class="five wide column">' +
-      '<div class="ui segment">' +
-      '<h2 class="ui header">Track Reseller</h2>' +
-      '</div>' +
-      '</div>' +
-      '</div>' +
-      '</div>')
+    const wrapper = shallow(<DisplaySourceDetail name={name} type={type} url={url} tag={tag} dateofCreate={dateofCreate} creator={creator} />)
+
+    expect(wrapper.html()).toEqual('<div class="ui main container">' +
+    '<div class="ui stackable grid">' +
+    '<div class="eleven wide column">' +
+    '<div class="ui segment">' +
+    '<iframe src="https://www.prontotools.io/" title="Track Reseller" frame-ancestors="none" width="100%" height="580px"></iframe>' +
+    '</div>' +
+    '</div>' +
+    '<div class="five wide column">' +
+    '<div class="ui segment">' +
+    '<h2 class="ui header">Track Reseller</h2>' +
+    '<div class="meta">' +
+    '<span class="ui left floated label">Reseller</span>' +
+    '</div>' +
+    '<div class="ui list meta">' +
+    '<div class="item">' +
+    '<i class="user circle outline big icon"></i>' +
+    '<div class="content">' +
+    '<div class="ui sub header">kan ouivirus</div>' +
+    '</div>' +
+    '</div>' +
+    '<b>created</b> ' +
+    '24, 01, 2018' +
+    '</div>' +
+    '</div>' +
+    '</div>' +
+    '</div>' +
+    '</div>')
   })
 })
