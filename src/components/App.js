0f6c613cc5b46e18316fc142105b04615c319be8
saowaluck <saowaluck@prontomarketing.com>
2018-02-24 22:06:36 +0700

    Fix app.js

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import 'semantic-ui-css/semantic.min.css'
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import AddSourceForm from './AddSourceForm'
+import DisplaySourceDetail from './../containers/DisplaySourceDetail'
+import Search from './Search'
+import EditSource from './../containers/EditSource'
+import Header from './Header'
+import Admin from './../backend/index'
+import './../assets/css/main.css'
+
+const App = () => (
+  <Router>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Search} />
+        <Route exact path='/Admin' component={Admin} />
+        <Route path='/resources/add/' component={AddSourceForm} />
+        <Route path='/resources/edit/:id' component={EditSource} />
+        <Route path='/resources/:id' component={DisplaySourceDetail} />
+      </Switch>
+    </div>
+  </Router>
+)
+
+export default App
