bee8621950c1cebdb040abe45efc7d314f5f6a96
KANOKWAN <kanokwan.kmk@gmail.com>
2018-03-28 01:25:00 +0700

    Edit test list all source. :peach:

--- src/components/__tests__/ListAllResource.test.js
+++ src/components/__tests__/ListAllResource.test.js
@@ -22,11 +22,10 @@ describe('< ListAllResource />', () => {
     const wrapper = shallow(<ListAllResource resource={resource} />)
     const expected = '<div class="ui row vertical segment"><div class="ui divided items">' +
     '<div class="item"><div class="content"><div class="ui row vertical"><h3 class="ui header">' +
-    '<a href="/resources/137/">Track Reseller</a><div class="ui right floated large button disabled">' +
+    '<a href="/resources/137/">Track Reseller</a><div class="ui right floated green large button disabled">' +
     '<div class="visible content">Knowledge Post</div></div></h3></div><br/><p><span>' +
-    '<i class="empty heart icon"></i>1</span>  <span><a href="/members/136/">' +
-    '<i class="user icon people"></i>saowaluck</a></span>  <span>' +
-    '<i class="wait icon"></i>Mar 23, 2018</span>  <span>' +
+    '<i class="empty heart icon"></i>1</span>  <span><a href="/members/136/"><i class="user icon people"></i>' +
+    'saowaluck</a></span>  <span><i class="wait icon"></i>Mar 23, 2018</span>  <span>' +
     '<i class="history icon"></i>Mar 23, 2018</span>  </p></div></div></div></div>'
     expect(wrapper.html()).toEqual(expected)
   })
