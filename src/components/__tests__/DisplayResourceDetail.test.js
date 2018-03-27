5e61474ce27021b5d2dbbf672f2474ba740f5bee
saowaluck <saowaluck@prontomarketing.com>
2018-03-27 20:14:54 +0700

    Fix test and add redux mock store

--- src/components/__tests__/DisplayResourceDetail.test.js
+++ src/components/__tests__/DisplayResourceDetail.test.js
@@ -62,23 +62,7 @@ describe('< DisplayResourceDetail />', () => {
 
     const wrapper = mount(<Provider store={store}><DisplayResourceDetail data={data} /></Provider>)
 
-    const expected = '<div class="ui main container">' +
-    '<div class="ui stackable grid">' +
-    '<div class="eleven wide column"><div class="ui segment">' +
-    '<iframe src="https://www.prontotools.io/" title="Track Reseller" frame-ancestors="none" width="100%" height="580px"></iframe>' +
-    '</div></div><div class="five wide column"><div class="ui segment"><h3 class="ui header">Track Reseller<a href="/resources/edit/0/">' +
-    '<i class="edit icon"></i></a></h3><div class="meta"><span class="ui left floated label">prontotool</span>' +
-    '<span class="ui left floated label">athena</span><i aria-hidden="true" class="icon ui heart outline icon large"></i>' +
-    '</div><hr><div class="ui list meta"><div class="item"><img class="ui mini circular image" alt="">' +
-    '<div class="content"><a href="/members/105/"><div class="ui sub header">kanokwan</div></a>Intern</div></div>' +
-    '<b>Created:</b> Thu Feb 15 2018 04:25:41<div class="accordion ui"><div class="title"><i aria-hidden="true" class="icon ui heart icon"></i>' +
-    'favorited by 1 protons<i aria-hidden="true" class="icon angle down icon"></i></div><div class="content"><div class="ui horizontal list">' +
-    '<div class="item"><a href="/members/0/"><img class="ui mini circular image" src="test.jpg" alt=""></a></div></div>' +
-    '</div></div></div><div class="ui row vertical segment"><h4 class="ui header"><b>Related Content</b></h4>' +
-    '<div class="ui middle aligned divided list"><div class="item"><div class="header">' +
-    '<a class="content" href="/resources/58/">test 1</a><span class="ui right floated label">Database</span></div></div>' +
-    '<div class="item"><div class="header"><a class="content" href="/resources/52/">test test table</a>' +
-    '<span class="ui right floated label">Database</span></div></div></div></div></div></div></div></div>'
+    const expected = ('<div class="ui main container"><div class="ui stackable grid"><div class="eleven wide column"><div class="ui segment"><iframe src="https://www.prontotools.io/" title="Track Reseller" frame-ancestors="none" width="100%" height="580px"></iframe></div></div><div class="five wide column"><div class="ui segment"><h3 class="ui header">Track Reseller<a href="/resources/0/edit/"><i class="edit icon"></i></a></h3><div class="meta"><span class="ui left floated label">prontotool</span><span class="ui left floated label">athena</span><i aria-hidden="true" class="icon ui heart outline icon large"></i></div><hr><div class="ui list meta"><div class="item"><img class="ui mini circular image" alt=""><div class="content"><a href="/members/105/"><div class="ui sub header">kanokwan</div></a>Intern</div></div><b>Created:</b> Thu Feb 15 2018 04:25:41<div class="accordion ui"><div class="title"><i aria-hidden="true" class="icon ui heart icon"></i>favorited by 1 protons<i aria-hidden="true" class="icon angle down icon"></i></div><div class="content"><div class="ui horizontal list"><div class="item"><a href="/members/0/"><img class="ui mini circular image" src="test.jpg" alt=""></a></div></div></div></div></div><div class="ui row vertical segment"><h4 class="ui header"><b>Related Content</b></h4><div class="ui middle aligned divided list"><div class="item"><div class="header"><a class="content" href="/resources/58/">test 1</a><span class="ui right floated label">Database</span></div></div><div class="item"><div class="header"><a class="content" href="/resources/52/">test test table</a><span class="ui right floated label">Database</span></div></div></div></div></div></div></div></div>')
     expect(wrapper.html()).toEqual(expected)
   })
 
@@ -110,25 +94,7 @@ describe('< DisplayResourceDetail />', () => {
       },
     }
     const wrapper = mount(<Provider store={store}><DisplayResourceDetail data={data} /></Provider>)
-    const expected = ('<div class="ui main container">' +
-    '<div class="ui stackable grid"><div class="eleven wide column">' +
-    '<div class="ui segment">' +
-    '<iframe src="https://www.prontotools.io/robot-framework-101-มาลองใช้กัน/" title="Robot Framework" frame-ancestors="none" width="100%" height="580px"></iframe>' +
-    '</div></div><div class="five wide column"><div class="ui segment">' +
-    '<h3 class="ui header">Robot Framework<a href="/resources/edit/0/">' +
-    '<i class="edit icon"></i></a></h3><div class="meta"><span class="ui left floated label">prontotool</span>' +
-    '<span class="ui left floated label">athena</span><i aria-hidden="true" class="icon ui heart outline icon large"></i></div>' +
-    '<hr><div class="ui list meta"><div class="item"><img class="ui mini circular image" alt=""><div class="content">' +
-    '<a href="/members/105/"><div class="ui sub header">kanokwan</div></a>Intern</div></div>' +
-    '<b>Created:</b> Thu Feb 15 2018 04:25:41<div class="accordion ui"><div class="title">' +
-    '<i aria-hidden="true" class="icon ui heart icon"></i>favorited by 1 protons' +
-    '<i aria-hidden="true" class="icon angle down icon"></i></div><div class="content"><div class="ui horizontal list">' +
-    '<div class="item"><a href="/members/0/"><img class="ui mini circular image" src="test.jpg" alt=""></a></div>' +
-    '</div></div></div></div><div class="ui row vertical segment"><h4 class="ui header"><b>Related Content</b></h4>' +
-    '<div class="ui middle aligned divided list"><div class="item"><div class="header">' +
-    '<a class="content" href="/resources/58/">test 1</a><span class="ui right floated label">Database</span></div></div>' +
-    '<div class="item"><div class="header"><a class="content" href="/resources/52/">test test table</a>' +
-    '<span class="ui right floated label">Database</span></div></div></div></div></div></div></div></div>')
+    const expected = ('<div class="ui main container"><div class="ui stackable grid"><div class="eleven wide column"><div class="ui segment"><iframe src="https://www.prontotools.io/robot-framework-101-มาลองใช้กัน/" title="Robot Framework" frame-ancestors="none" width="100%" height="580px"></iframe></div></div><div class="five wide column"><div class="ui segment"><h3 class="ui header">Robot Framework<a href="/resources/0/edit/"><i class="edit icon"></i></a></h3><div class="meta"><span class="ui left floated label">prontotool</span><span class="ui left floated label">athena</span><i aria-hidden="true" class="icon ui heart outline icon large"></i></div><hr><div class="ui list meta"><div class="item"><img class="ui mini circular image" alt=""><div class="content"><a href="/members/105/"><div class="ui sub header">kanokwan</div></a>Intern</div></div><b>Created:</b> Thu Feb 15 2018 04:25:41<div class="accordion ui"><div class="title"><i aria-hidden="true" class="icon ui heart icon"></i>favorited by 1 protons<i aria-hidden="true" class="icon angle down icon"></i></div><div class="content"><div class="ui horizontal list"><div class="item"><a href="/members/0/"><img class="ui mini circular image" src="test.jpg" alt=""></a></div></div></div></div></div><div class="ui row vertical segment"><h4 class="ui header"><b>Related Content</b></h4><div class="ui middle aligned divided list"><div class="item"><div class="header"><a class="content" href="/resources/58/">test 1</a><span class="ui right floated label">Database</span></div></div><div class="item"><div class="header"><a class="content" href="/resources/52/">test test table</a><span class="ui right floated label">Database</span></div></div></div></div></div></div></div></div>')
     expect(wrapper.html()).toEqual(expected)
   })
 })
