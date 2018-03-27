867a328b40f083129b9957ee85b69ef561563956
KANOKWAN <kanokwan.kmk@gmail.com>
2018-03-28 00:04:23 +0700

    Check add resource duplicated. :peach:

--- src/routes/Resource.js
+++ src/routes/Resource.js
@@ -69,24 +69,27 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
   let resources = []
+  let resourceId = 'undefined'
   const member = await Member.getMemberByEmail(req.body.email)
   resources = await Resource.createResource(req.body)
-  const resourceId = Number(resources.id)
-  const memberId = Number(member.id)
-  resources = await Resource.memberCreatedResource(resourceId, memberId)
-  const { tags } = req.body
-  if (tags !== '') {
-    const tagsCleaned = cleanTags(tags)
-    resources = await Promise.all(tagsCleaned.map(async (tag) => {
-      let result = await Tag.getTagsByName(tag)
-      if (result.records.length === 0) {
-        result = await Resource.createResourceHasTag(resourceId, tag)
-      } else {
-        const tagId = result.records[0]._fields[0].identity.low
-        result = await Resource.createResourceHasTagDuplicate(resourceId, tagId)
-      }
-      return result
-    }))
+  if (resources.length !== 0) {
+    resourceId = Number(resources.id)
+    const memberId = Number(member.id)
+    resources = await Resource.memberCreatedResource(resourceId, memberId)
+    const { tags } = req.body
+    if (tags !== '') {
+      const tagsCleaned = cleanTags(tags)
+      resources = await Promise.all(tagsCleaned.map(async (tag) => {
+        let result = await Tag.getTagsByName(tag)
+        if (result.records.length === 0) {
+          result = await Resource.createResourceHasTag(resourceId, tag)
+        } else {
+          const tagId = result.records[0]._fields[0].identity.low
+          result = await Resource.createResourceHasTagDuplicate(resourceId, tagId)
+        }
+        return result
+      }))
+    }
   }
   res.json({ id: resourceId })
 })
